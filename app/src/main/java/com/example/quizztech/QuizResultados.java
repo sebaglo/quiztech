c0b8c59104e77616bd82311283c681c5e3ecb9af
sebaglo <cardenas2araya@gmail.com>
2024-09-29 22:21:41 -0300

    versiones

--- app/src/main/java/com/example/quizztech/QuizResultados.java
+++ app/src/main/java/com/example/quizztech/QuizResultados.java
@@ -1,9 +1,13 @@
 package com.example.quizztech;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -16,5 +20,30 @@ public class QuizResultados extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_quiz_resultados);
 
+
+        final AppCompatButton startNewBtn = findViewById(R.id.btnVolverjugar);
+        final TextView correctAnswer = findViewById(R.id.respuestaCorrecta);
+        final TextView IncorrectAnswer = findViewById(R.id.respuestaIncorrecta);
+
+        final int getCorrectAnswer = getIntent().getIntExtra("Respuestas Correctas", 0);
+        final  int getIncorrectAnswer = getIntent().getIntExtra("Respuestas Incorrectas", 0);
+
+        correctAnswer.setText(String.valueOf(getCorrectAnswer));
+        IncorrectAnswer.setText(String.valueOf(getIncorrectAnswer));
+
+        startNewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(QuizResultados.this, MainActivity.class));
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(QuizResultados.this, MainActivity.class));
+        finish();
     }
 }
